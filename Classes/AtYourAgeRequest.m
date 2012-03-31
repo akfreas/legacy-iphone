32471ed2c2e01ca009811f573bc5eedf137bddf1
Alex Freas <alexander.freas@agilex.com>
2012-03-31 01:19:51 -0400

    Gotta love dropbox. Restored atyouragerequest.

--- Classes/AtYourAgeRequest.m
+++ Classes/AtYourAgeRequest.m
@@ -1,13 +1,56 @@
-//
-//  AtYourAgeRequest.m
-//  AtYourAge
-//
-//  Created by Alexander Freas on 3/30/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import "AtYourAgeRequest.h"
+#import "Event.h"
 
-@implementation AtYourAgeRequest
+@implementation AtYourAgeRequest {
+    NSURLRequest *request;
+    Class requestClass;
+}
+
++(NSURL *)baseUrl {
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"AtYourAge-Info" ofType:@"plist"];
+    NSDictionary *plistDict = [NSDictionary dictionaryWithContentsOfFile:path];
+    NSURL *url = [NSURL URLWithString:[plistDict objectForKey:@"ResourceUrl"]];
+    return url;
+}
+
++(NSURL *)appendToBaseUrl:(NSString *)theUrl {
+    
+    NSURL *url = [[AtYourAgeRequest baseUrl] URLByAppendingPathComponent:theUrl];
+    return url;
+}
+
++(AtYourAgeRequest *)requestToGetEventWithBirthday:(NSDate *)birthday {
+
+    NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] init];
+    NSURL *url = [AtYourAgeRequest appendToBaseUrl:@"/events"];
+    [urlRequest setURL:url];
+    [urlRequest setHTTPMethod:@"GET"];
+    [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    
+    AtYourAgeRequest *request = [[AtYourAgeRequest alloc] initWithRequest:urlRequest classToParse:[Event class]];
+    
+    return request;
+}
+
+-(id)initWithRequest:(NSURLRequest *)theRequest {
+    self = [super init];
+    
+    if (self) {
+        request = theRequest;
+    }
+    
+    return self;
+}
+
+-(id)initWithRequest:(NSURLRequest *)theRequest classToParse:(Class)theClass {
+    self = [self initWithRequest:theRequest];
+    
+    if (self) {
+        requestClass = theClass;
+    }
+    
+    return self;
+}
 
 @end

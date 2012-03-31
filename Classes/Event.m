0e3bd6174aadd88c459798d040f717e2bfd96d0b
Alex Freas <alexander.freas@agilex.com>
2012-03-31 02:16:19 -0400

    Added initwithjsondictionary to event.

--- Classes/Event.m
+++ Classes/Event.m
@@ -1,5 +1,21 @@
 #import "Event.h"
 
+static NSString *KeyForName = @"name";
+static NSString *KeyForDescription = @"description";
+
 @implementation Event
+
+@synthesize name;
+@synthesize description;
+
+-(id)initWithJsonDictionary:(NSDictionary *)jsonDict {
+    self = [super init];
+    
+    if (self) {
+        description = [jsonDict objectForKey:KeyForDescription];
+        name = [jsonDict objectForKey:KeyForName];
+    }
+    return self;
+}
 
 @end

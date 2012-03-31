a22c89c8a50db6ce4c0dabb5cc082e7a7690acc8
Alex Freas <alexander.freas@agilex.com>
2012-03-30 21:01:07 -0400

    Initial Commit

---
+++ AtYourAge/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AtYourAge
+//
+//  Created by Alexander Freas on 3/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

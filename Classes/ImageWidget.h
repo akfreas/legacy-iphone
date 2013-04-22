8f380e38c3e2b501b8c6dcd7635f836bb05dc866
Alex Freas <alex@taximagic.com>
2013-04-22 09:22:34 -0400

    Imagewidget now working well.

--- Classes/ImageWidget.h
+++ Classes/ImageWidget.h
@@ -3,5 +3,8 @@
 -(id)initWithSmallImage:(UIImage *)theSmallImage largeImage:(UIImage *)theLargeImage;
 
 @property (nonatomic, assign) CGFloat angle;
+@property (nonatomic, assign) CGFloat smallImageRadius;
+@property (nonatomic) UIImage *smallImage;
+@property (nonatomic) UIImage *largeImage;
 
 @end
--- Classes/ImageWidget.m
+++ Classes/ImageWidget.m
@@ -1,52 +1,27 @@
 #import "ImageWidget.h"
 
-
-#define Ax	0
-#define Ay	0
-#define Bx	100
-#define By	0
-#define Cx	100
-#define Cy	100
-#define Dx	0
-#define Dy	100
-#define Ex	50
-#define Ey	10
-#define Fx	10
-#define Fy	90
-#define Gx	90
-#define Gy	90
-#define Hx	50
-#define Hy	90
-#define Ix	50
-#define Iy	100
-
-
-@implementation ImageWidget {
-    
-    UIImage *largeImage;
-    UIImage *smallImage;
-    
-}
+@implementation ImageWidget
 
 
 -(id)initWithSmallImage:(UIImage *)theSmallImage largeImage:(UIImage *)theLargeImage {
-    self = [self initWithFrame:CGRectMake(0, 0, 100, 100)];
+    self = [self init];
     
     if (self) {
-        largeImage = theLargeImage;
-        smallImage = theSmallImage;
-        _angle = 135;
-        self.backgroundColor = [UIColor clearColor];
+        _largeImage = theLargeImage;
+        _smallImage = theSmallImage;
     }
-    
     return self;
 }
 
 
-
--(id)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
+-(id)init {
     
+    self = [super initWithFrame:CGRectMake(0, 0, 100, 100)];
+    if (self) {
+        _angle = 135;
+        _smallImageRadius = 50;
+        self.backgroundColor = [UIColor clearColor];
+    }
     return self;
 }
 
@@ -55,17 +30,32 @@
     [self setNeedsDisplay];
 }
 
+-(void)setSmallImageRadius:(CGFloat)smallImageRadius {
+    _smallImageRadius = smallImageRadius;
+    [self setNeedsDisplay];
+}
+
+-(void)setSmallImage:(UIImage *)smallImage {
+    _smallImage = smallImage;
+    [self setNeedsDisplay];
+}
+
+-(void)setLargeImage:(UIImage *)largeImage {
+    _largeImage = largeImage;
+    [self setNeedsDisplay];
+}
+
 
 
 -(void)drawRect:(CGRect)rect {
     
     
-    CGSize imgSize = largeImage.size;
+    CGSize imgSize = _largeImage.size;
     CGSize viewSize = rect.size;
     CGRect imgRect;
     
     CGFloat scale = MIN(viewSize.width / imgSize.width, viewSize.height / imgSize.height);
-    imgRect = CGRectMake(0, 0, largeImage.size.width * scale, largeImage.size.height * scale);
+    imgRect = CGRectMake(0, 0, _largeImage.size.width * scale, _largeImage.size.height * scale);
     
     CGContextRef context = UIGraphicsGetCurrentContext();
 //    CGContextFillRect(context, rect);
@@ -80,27 +70,24 @@
     CGContextEOClip(context);
     
     
+//    _smallImage = _largeImage;
     
-    
-    CGContextDrawImage(context, imgRect, [largeImage CGImage]);
+    CGContextDrawImage(context, imgRect, [_largeImage CGImage]);
     CGContextRestoreGState(context);
     
     CGContextSaveGState(context);
-//    CGContextMoveToPoint(context, imgRect.size.width, imgRect.size.height);
-    CGSize smImgSize = smallImage.size;
-    scale = MIN(50 / smImgSize.width, 50 / smImgSize.height);
+    CGSize smImgSize = _smallImage.size;
+    scale = MIN(_smallImageRadius / smImgSize.width, _smallImageRadius / smImgSize.height);
     
     
     CGPoint arcCenter = CGPointMake((imgRect.size.width / 2) - largeArcRadius * cos(_angle * M_PI/180), imgRect.size.height / 2 -  largeArcRadius * sin(_angle * M_PI / 180));
-    CGFloat smImgWidth = smallImage.size.width * scale;
-    CGFloat smImgHeight = smallImage.size.height * scale;
+    CGFloat smImgWidth = _smallImage.size.width * scale;
+    CGFloat smImgHeight = _smallImage.size.height * scale;
     CGRect smImgRect = CGRectMake(arcCenter.x - smImgWidth / 2, arcCenter.y - smImgHeight / 2, smImgWidth, smImgHeight);
     CGContextAddArc(context, arcCenter.x, arcCenter.y, (MIN(smImgSize.height, smImgSize.width) * scale) / 2, 0, 2*M_PI, 1);
     CGContextClip(context);
-    CGContextDrawImage(context, smImgRect, smallImage.CGImage);
+    CGContextDrawImage(context, smImgRect, _smallImage.CGImage);
     CGContextRestoreGState(context);
-    
-    
 }
 
 @end

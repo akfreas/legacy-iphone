fb29ce321109957fb96c4f9060de7a256e06eeec
Alex Freas <akfreas@gmail.com>
2013-07-02 18:43:00 -0400

    using afnetworking in connection.

--- Classes/AtYourAgeConnection.m
+++ Classes/AtYourAgeConnection.m
@@ -1,5 +1,5 @@
 #import "Event.h"
-
+#import "AFNetworking.h"
 #import "AtYourAgeConnection.h"
 @implementation AtYourAgeConnection {
     
@@ -21,47 +21,12 @@
 
 -(void)getWithCompletionBlock:(void(^)(AtYourAgeRequest *request, id result, NSError *error))_block {
     
-    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request.urlRequest delegate:self startImmediately:NO];
-    self.AYAConnectionCallback = _block;
-    
-    [connection scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
-    [connection start];
-}
-
--(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
-    
-    if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
-        
-        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-        
-        if (httpResponse.statusCode != 200) {
-            
-            NSError *error = [NSError errorWithDomain:@"Status Code Error" 
-                                                 code:httpResponse.statusCode
-                                             userInfo:[NSDictionary dictionaryWithObject:[NSString stringWithFormat:@"Status code %d", httpResponse.statusCode]
-                                                                                  forKey:NSLocalizedDescriptionKey]];
-            self.AYAConnectionCallback(nil, nil, error);
-        }
-        resultData = [[NSMutableData alloc] initWithLength:0];
-    }
-}
-
--(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-    [resultData appendData:data];
-}
-
--(void)connectionDidFinishLoading:(NSURLConnection *)connection {
-    
-    result = nil;
-    NSError *error;
-    NSString *str = [[NSString alloc] initWithData:resultData encoding:NSUTF8StringEncoding];
-    id parseResult = [NSJSONSerialization JSONObjectWithData:resultData options:NSJSONReadingMutableLeaves error:&error];
-    if (error) {
-        NSLog(@"Error: %@, str: %@", error, str);
-    }
-//    NSLog(@"parse result: %@", parseResult);
-    result = parseResult;
-    self.AYAConnectionCallback(request, result, nil);
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request.urlRequest success:^(NSURLRequest *URLRequest, NSHTTPURLResponse *response, id JSON) {
+        _block(request, JSON, NULL);
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+        NSLog(@"Json: %@ Error: %@ response:%@", JSON, error, response);
+    }];
+    [operation start];
 }
 
 @end

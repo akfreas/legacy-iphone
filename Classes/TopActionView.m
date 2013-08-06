2b846000ba58de163b5c1c8d01f38bf0f588412c
Alex Freas <akfreas@gmail.com>
2013-08-06 13:32:26 -0400

    TopACtionView now has a facebook profile pic.

--- Classes/TopActionView.m
+++ Classes/TopActionView.m
@@ -1,8 +1,12 @@
 #import "CircleImageView.h"
 #import "TopActionView.h"
+#import "ObjectArchiveAccessor.h"
+#import "Person.h"
 
 @implementation TopActionView {
     CircleImageView *addFriendsButton;
+    CircleImageView *profilePicButton;
+    CGFloat buttonRadii;
 }
 
 - (id)initWithFrame:(CGRect)frame
@@ -10,18 +14,29 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor blackColor];
+        buttonRadii = self.bounds.size.height / 2 - 5;
         [self addAddFriendsButton];
+        [self addProfilePicButton];
     }
     return self;
 }
 
 -(void)addAddFriendsButton {
     
-    addFriendsButton = [[CircleImageView alloc] initWithImage:[UIImage imageNamed:@"10-medical.png"] radius:self.bounds.size.height / 2 - 5];
-    addFriendsButton.frame = CGRectMakeFrameForDeadCenterInRect(self.bounds, addFriendsButton.frame.size);
+    addFriendsButton = [[CircleImageView alloc] initWithImage:[UIImage imageNamed:@"10-medical.png"] radius:buttonRadii];
+    addFriendsButton.frame = CGRectMake(self.frame.size.width - buttonRadii*2 - 5, 5, addFriendsButton.frame.size.height, addFriendsButton.frame.size.width);
     UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(addFriendsButtonTappedAction)];
     [addFriendsButton addGestureRecognizer:gesture];
     [self addSubview:addFriendsButton];
+}
+
+-(void)addProfilePicButton {
+    ObjectArchiveAccessor *accessor = [[ObjectArchiveAccessor alloc] init];
+    Person *mainPerson = [accessor primaryPerson];
+    UIImage *thumb = [UIImage imageWithData:mainPerson.thumbnail];
+    profilePicButton = [[CircleImageView alloc] initWithImage:thumb radius:buttonRadii];
+    profilePicButton.frame = CGRectMake(5, 5, profilePicButton.frame.size.width, profilePicButton.frame.size.height);
+    [self addSubview:profilePicButton];
 }
 
 -(void)addFriendsButtonTappedAction {

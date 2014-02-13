f527f3f60dc5b090b406a76511583c10e05dfc1c
Alex Freas <akfreas@gmail.com>
2014-02-13 09:33:22 -0500

    [data sync] Made array parsing function public so we can test datasync's functionality.

--- Classes/DataSyncUtility.h
+++ Classes/DataSyncUtility.h
@@ -1,10 +1,12 @@
-@interface DataSyncUtility : NSObject
+@class Figure;
 
+@interface DataSyncUtility : NSObject
 
 +(DataSyncUtility *)sharedInstance;
 
 -(void)sync:(void(^)())completion;
 -(void)syncFacebookFriends:(void(^)())completion;
 +(void)syncRelatedEventForFigure:(Figure *)figure;
+-(void)parseArrayOfEventsForTable:(NSArray *)events;
 
 @end

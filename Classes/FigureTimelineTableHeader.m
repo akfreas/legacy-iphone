ace9e99f986d213ef8e67527158c76ec3400b922
Alex Freas <akfreas@gmail.com>
2014-02-21 09:39:01 -0500

    [figure timeline header] Using notification utils to scroll to pages.

--- Classes/FigureTimelineTableHeader.m
+++ Classes/FigureTimelineTableHeader.m
@@ -54,14 +54,14 @@
     [leftButton setImage:[UIImage imageNamed:@"back-arrow"] forState:UIControlStateNormal];
     [self.contentView addSubview:leftButton];
     [leftButton bk_addEventHandler:^(id sender) {
-        [AKNOTIF postNotificationName:KeyForScrollToPageNotification object:nil userInfo:@{KeyForPageNumberInUserInfo : [NSNumber numberWithInteger:0]}];
+        [NotificationUtils scrollToPage:LandingPageNumber];
     } forControlEvents:UIControlEventTouchUpInside];
     
     rightButton = [[UIButton alloc] initForAutoLayout];
     [rightButton setImage:[UIImage imageNamed:@"next-arrow"] forState:UIControlStateNormal];
     [self.contentView addSubview:rightButton];
     [rightButton bk_addEventHandler:^(id sender) {
-        [AKNOTIF postNotificationName:KeyForScrollToPageNotification object:nil userInfo:@{KeyForPageNumberInUserInfo: [NSNumber numberWithInteger:2]} ];
+        [NotificationUtils scrollToPage:WebViewPageNumber];
     } forControlEvents:UIControlEventTouchUpInside];
     
 }

5e50a11af608ea411370c6e08bc998dfaf107107
Alex Freas <akfreas@gmail.com>
2014-02-21 09:40:37 -0500

    [info page] No nibs, updated with new design.

--- Classes/LegacyInfoPage.h
+++ Classes/LegacyInfoPage.h
@@ -1,5 +1,8 @@
 #include "PageProtocol.h"
 
+#define AKDictionaryOfVariableBindings(...) _AKDictionaryOfVariableBindings(@"" # __VA_ARGS__, __VA_ARGS__, nil)
+NSDictionary * _AKDictionaryOfVariableBindings(NSString *commaSeparatedKeysString, id firstValue, ...);
+
 @interface LegacyInfoPage : UIView <PageProtocol>
 
 @end
--- Classes/LegacyInfoPage.m
+++ Classes/LegacyInfoPage.m
@@ -3,66 +3,141 @@
 
 #import <MessageUI/MessageUI.h>
 
+@interface LegacyInfoPage ()
+
+@property FacebookSignInButton *facebookButton;
+@property UIButton *feedbackButton;
+@property UILabel *facebookText;
+@property UIButton *backButton;
+@property UIImageView *logoImage;
+@property UILabel *appDescriptionLabel;
+
+
+@end
+
 @implementation LegacyInfoPage {
-    
-    IBOutlet FacebookSignInButton *facebookButton;
-    IBOutlet UILabel *pushNotificationLabel;
-    IBOutlet UIButton *sendMailButton;
-    IBOutlet UITextView *facebookText;
-}
+    }
 
 - (id)initWithFrame:(CGRect)frame
 {
-    self = [[NSBundle mainBundle] loadNibNamed:NSStringFromClass(self.class) owner:self options:nil][0];
+    self = [super initWithFrame:frame];
     if (self) {
         self.frame = frame;
-        pushNotificationLabel.font = [UIFont fontWithName:@"Cinzel-Regular" size:16.0f];
         if ([MFMailComposeViewController canSendMail] == NO) {
-            sendMailButton.hidden = YES;
+            self.feedbackButton.hidden = YES;
         }
 //        [self checkIfPushEnabledAndFlipSwitch];
+        self.backgroundColor = AppLightBlueColor;
         [self removeFacebookButtonIfAuthorized];
+        [self addUIComponents];
     }
     return self;
 }
 
-//-(void)checkIfPushEnabledAndFlipSwitch {
-//    
-//    UIRemoteNotificationType types = [[UIApplication sharedApplication] enabledRemoteNotificationTypes];
-//    
-//    if (types == UIRemoteNotificationTypeNone) {
-//        notificationSwitch.on = NO;
-//    } else {
-//        notificationSwitch.on = YES;
-//    }
-//    
-//}
-
 -(void)removeFacebookButtonIfAuthorized {
     if ([FBSession activeSession].state != FBSessionStateCreatedTokenLoaded) {
-        facebookButton.hidden = facebookText.hidden = NO;
+        self.facebookButton.hidden = self.facebookText.hidden = NO;
     } else {
-        facebookButton.hidden = facebookText.hidden = YES;
+        self.facebookButton.hidden = self.facebookText.hidden = YES;
     }
 }
 
 
-#pragma mark IBActions
-
--(IBAction)sendMessageButton:(id)sender {
-    [Flurry logEvent:@"send_message_button_tapped"];
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"sendMail" object:nil];
+-(void)addUIComponents {
+    [self addBackButton];
+    [self addLogoImage];
+    [self addAppDescriptionLabel];
+    [self addFeedbackButton];
+    [self addFacebookTextLabel];
+    [self addFacebookButton];
+    [self addLayoutConstraints];
 }
 
--(IBAction)notificationSwitch:(UISwitch *)theSwitch {
+-(void)addBackButton {
+    self.backButton = [[UIButton alloc] initForAutoLayout];
+    [self.backButton setImage:CloseButtonImage forState:UIControlStateNormal];
+    [self.backButton bk_addEventHandler:^(id sender) {
+        [NotificationUtils scrollToPage:LandingPageNumber];
+    } forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview:self.backButton];
+}
+
+-(void)addLogoImage {
+    self.logoImage = [[UIImageView alloc] initWithImage:LogoImage];
+    [self addSubview:self.logoImage];
+}
+
+-(void)addAppDescriptionLabel {
+    self.appDescriptionLabel = [[UILabel alloc] initForAutoLayout];
+    self.appDescriptionLabel.text = @"Discover the past milestones of prominent people.";
+    self.appDescriptionLabel.textColor = [UIColor whiteColor];
+    self.appDescriptionLabel.font = InfoPageFont;
+    self.appDescriptionLabel.numberOfLines = 2;
+    [self addSubview:self.appDescriptionLabel];
+}
+
+-(void)addFeedbackButton {
+    self.feedbackButton = [[UIButton alloc] initForAutoLayout];
+    self.feedbackButton.layer.cornerRadius = 8;
+    self.feedbackButton.backgroundColor = LightButtonColor;
+    self.feedbackButton.titleLabel.font = InfoPageFont;
+    [self.feedbackButton setTitle:@"Feedback" forState:UIControlStateNormal];
+    [self.feedbackButton bk_addEventHandler:^(id sender) {
+        [Flurry logEvent:@"send_message_button_tapped"];
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"sendMail" object:nil];
+    } forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview:self.feedbackButton];
+}
+
+-(void)addFacebookTextLabel {
+    self.facebookText = [[UILabel alloc] initForAutoLayout];
+    self.facebookText.text = @"We welcome you to connect your Facebook profile to compare your accomplishments to those who left a legacy.";
+    self.facebookText.font = InfoPageFont;
+    self.facebookText.textColor = [UIColor whiteColor];
+    self.facebookText.numberOfLines = 6;
+    [self addSubview:self.facebookText];
+}
+
+-(void)addFacebookButton {
+    self.facebookButton = [[FacebookSignInButton alloc] initForAutoLayout];
     
-    if (theSwitch.on == YES) {
-        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)];
-    } else {
-        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:UIRemoteNotificationTypeNone];
-    }
+    [self addSubview:self.facebookButton];
 }
 
+-(void)addLayoutConstraints {
+    
+    NSDictionary *bindingDict = @{@"facebookText" : self.facebookText,
+                           @"backButton" : self.backButton,
+                           @"logoImage" : self.logoImage,
+                           @"appDescriptionLabel" : self.appDescriptionLabel,
+                           @"feedbackButton" : self.feedbackButton,
+                           @"facebookButton" : self.facebookButton};
+    for (UIView *v in [bindingDict allValues]) {
+        v.translatesAutoresizingMaskIntoConstraints = NO;
+    }
+    [self addConstraintWithVisualFormat:@"V:|-(25)-[backButton]-(<=30)-[logoImage]-(<=45)-[appDescriptionLabel]-(15)-[feedbackButton(25)]" bindings:bindingDict];
+    [self addConstraintWithVisualFormat:@"H:|-[backButton]" bindings:bindingDict];
+    [self.logoImage autoAlignAxisToSuperviewAxis:ALAxisVertical];
+    [self.appDescriptionLabel autoAlignAxisToSuperviewAxis:ALAxisVertical];
+    [self addConstraintWithVisualFormat:@"H:|-[appDescriptionLabel]-|" bindings:bindingDict];
+    [self addConstraintWithVisualFormat:@"H:|-[feedbackButton(100)]" bindings:bindingDict];
+    
+    
+    [self addConstraintWithVisualFormat:@"V:[feedbackButton]-(15)-[facebookText]-(>=10)-[facebookButton]-|" bindings:bindingDict];
+    [self addConstraintWithVisualFormat:@"H:|-[facebookText]-|" bindings:bindingDict];
+    [self addConstraintWithVisualFormat:@"H:|-[facebookButton]-|" bindings:bindingDict];
+}
+
+
+NSDictionary * _AKDictionaryOfVariableBindings(NSString *commaSeparatedKeysString, id firstValue, ...) {
+    NSArray *keys = [commaSeparatedKeysString componentsSeparatedByString:@","];
+    va_list listPtr;
+    va_start(listPtr, [keys count]);
+    NSMutableDictionary *dict = [NSMutableDictionary new];
+    for (int i=0; i<[keys count]; i++) {
+        
+    }
+}
 
 #pragma mark PageProtocol Delegate Methods
 
